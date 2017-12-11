a42da32bed1eb6ed377872f6cb6d04a900df8e0a
lss <81438234@qq.com>
2017-12-11 09:55:39 +0800

    Update global.js

--- src/global/global.js
+++ src/global/global.js
@@ -1,58 +1,26 @@
 import Vue from 'vue';
 import VueResource from 'vue-resource';
+import Cookies from 'js-cookie';
+
 Vue.use(VueResource);
 
-import { Loading } from 'element-ui';
+if(Cookies.get('auth-token')){
+  Vue.http.headers.common['auth-token'] = 'Bearer.'+Cookies.get('auth-token');
+}else{
+  
+}
 
-import Cookies from 'js-cookie';
+import { Indicator } from 'mint-ui';
+
 // const config = require('../../config');
 import config from '../../config';
 
 //封装一些全局元素。如全站通用功能函数和http请求等
 
-export  const global = {
-        // 测试环境
-        // baseUrl:"http://120.76.119.16:8888/jojosns/",
-        // baseImgUrl:"http://120.76.119.16:8888/jojosns/",
-        // 开发环境
-        baseUrl: "http://27.61.80.50:8080/jojosns/",
-        baseImgUrl: "http://27.61.80.50:8080/jojosns/", //图片服务器，若没有单独服务，可忽略此项
-
-        staticPath: process.env.NODE_ENV !== 'development' ?  config.build.staticPath: config.dev.staticPath,//静态资源路径
-        /**
-         * 切换主题函数
-         */
-        changeTheme:function(themeValue){
-            var that = this;
-            // console.log('切换主题颜色值：',themeValue,that.staticPath,JSON.stringify(themeArray) );
-            //需要移到单独的文件存放
-            var cssArray = themeArray;
-
-            for (let i = 0 ,len = cssArray.length; i < len; i++) {
-                var itemPath = that.staticPath+'/theme/'+themeValue+'/'+cssArray[i].toLowerCase()+'.css';
-                
-                loadCss(itemPath)
-            };
-            
-            localStorage.setItem("themeValue",themeValue)
-
-            function loadCss(path){
-                var head = document.getElementsByTagName('head')[0];
-                var link = document.createElement('link');
-                    link.href = path;
-                    link.rel = 'stylesheet';
-                    link.type = 'text/css';
-                    head.appendChild(link);
-              
-            }
-        },
-        /**
-         * 全局ID计数器，保证返回的是一个全局的id标识（数字）
-         */
-        globalId: function () {
-            return window._idCounter_ ? window._idCounter_ += 1 : window._idCounter_ = 1;
-        },
+  const global = {
         
+        staticPath: process.env.NODE_ENV !== 'development' ?  config.build.staticPath: config.dev.staticPath,//静态资源路径
+       
         /**
          * 统一接口处理：get请求方法封装。this.$http.get(url, [options])
          * @param url { String } -必选 接口url
@@ -73,8 +41,12 @@ export  const global = {
 
             //遮罩层
             if(isLoading){
-                var loadingInstance = Loading.service({text:"拼命加载中"});
+                Indicator.open({
+                  //text: '加载中...',
+                  spinnerType: 'fading-circle'
+                });
             }
+            // options.headers['auth-token'] = Cookies.get('auth-token');
             Vue.http.get(url, options).then((response) => {
                 // 响应成功回调
                 //console.log('成功回调')
@@ -83,10 +55,10 @@ export  const global = {
                 setTimeout(function(){
                     sucCb(response);
                     if(isLoading){
-                        loadingInstance.close();
+                        Indicator.close();
                     }
                     
-                },1000)
+                },500)
                    
                 
             }, (response) => {
@@ -94,7 +66,7 @@ export  const global = {
                 //console.log('失败回调')
                 errorCb(response);
                 if(isLoading){
-                   loadingInstance.close();
+                   Indicator.close();
                 }
             })
         },
@@ -119,22 +91,28 @@ export  const global = {
 
             //遮罩层
             if(isLoading){
-                var loadingInstance = Loading.service();
+                Indicator.open({
+                  //text: '加载中...',
+                  spinnerType: 'fading-circle'
+                });
             }
             Vue.http.post(url,body,options).then((response) => {
                 // 响应成功回调
                 //console.log('成功回调')
-                sucCb(response);
-                if(isLoading){
-                   loadingInstance.close();
-                }
+                setTimeout(function(){
+                    sucCb(response);
+                    if(isLoading){
+                        Indicator.close();
+                    }
+                    
+                },500)
                  
             }, (response) => {
                 // 响应错误回调
                 //console.log('失败回调')
                 errorCb(response);
                 if(isLoading){
-                   loadingInstance.close();
+                   Indicator.close();
                 }
             })
         },
@@ -146,7 +124,7 @@ export  const global = {
          * 日期格式化,传入为毫秒数,转出时间格式为 ：2016-6-6 12:00:00
          * @objD 必填，格式为毫秒数
          */
-        formatDate: function (objD) {
+        formatDate: function (objD,cFormat) {
             if (!objD) {
                 return '';
             }
@@ -165,30 +143,11 @@ export  const global = {
             if (mm < 10) mm = '0' + mm;
             var ss = objD.getSeconds();
             if (ss < 10) ss = '0' + ss;
-            str = yy + "-" + MM + "-" + dd + " " + hh + ":" + mm + ":" + ss;
-            return (str);
-        },
-        /**
-         * author lss
-         * 日期格式化,传入为毫秒数,转出时间格式为 ：2016-6-6
-         * @objD 必填，格式为毫秒数
-         */
-        formatDate2: function (objD) {
-            if (!objD) {
-                return '';
-            }
-            objD = new Date(objD);
-            var str;
-            var yy = objD.getYear();
-            if (yy < 1900) yy = yy + 1900;
-            var MM = objD.getMonth() + 1;
-            //if(MM<10) MM = '0' + MM;
-            var dd = objD.getDate();
-            //if(dd<10) dd = '0' + dd;
-            //去掉0 我的圈子 加入圈子，布局放不下。。
+            // str = yy + "-" + MM + "-" + dd + " " + hh + ":" + mm + ":" + ss;
             str = yy + "-" + MM + "-" + dd;
             return (str);
         },
+       
 		
         
         /**
@@ -225,4 +184,4 @@ export  const global = {
     };
     
 
-// export default global
+export default global
